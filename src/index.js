f47e88f2bf26920cd73b0aa9c25937128a95bc13
Karasova Yuliia <julia.povolotskaya@gmail.com>
2022-08-22 19:33:53 +0200

    Add function Forecast

--- src/index.js
+++ src/index.js
@@ -13,6 +13,41 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thur", "Fri", "Sat", "Sun"];
+    days.forEach(function(day) {
+        forecastHTML = 
+        forecastHTML + 
+        `
+        
+           <div class="col-2">
+            <div class="weather-forecast-date">
+                ${day}
+            </div>
+    
+    <img src="http://openweathermap.org/img/wn/10d@2x.png" width="36px">
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">
+                    18°
+                </span>
+                <span class="weather-forecast-temperature-min">
+                    14°
+                </span>
+              </div>
+           </div>
+        
+        `;
+    });
+    
+    
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+    
+}
+
 function displayTemperature(response) {
     console.log(response.data);
     console.log(response.data.main.temp);
@@ -82,3 +117,5 @@ let celsiusitLink = document.querySelector("#celsius-link");
 celsiusitLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+
+displayForecast();
